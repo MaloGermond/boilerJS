879051b07815a2a140da73804b1d8f23eef6cfc7
MaloGermond <github@matchalo.org>
2024-01-25 12:25:58 +0100

    :tada: Starting the projec
    starting the project with basic stuff

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs-extra');
+const inquirer = require('inquirer');
+
+async function copyTemplate() {
+  // Ask for repository name and file types
+  const answers = await inquirer.prompt([{
+      type: 'input',
+      name: 'repositoryName',
+      message: 'Enter the repository name:',
+    },
+    {
+      type: 'checkbox',
+      name: 'fileTypes',
+      message: 'Select the file types required:',
+      choices: ['html', 'css', 'js'],
+    },
+  ]);
+
+  const { repositoryName, fileTypes } = answers;
+
+  try {
+    // Copy files from the template directory to the current repository
+    await fs.copy('./template', `./${repositoryName}`);
+
+    // Optionally, filter and keep only selected file types
+    if(fileTypes.length > 0) {
+      const files = await fs.readdir(`./${repositoryName}`);
+      const filteredFiles = files.filter(file => fileTypes.includes(file.split('.')
+        .pop()));
+      await Promise.all(filteredFiles.map(file => fs.remove(`./${repositoryName}/${file}`)));
+    }
+
+    console.log('Boilerplate created successfully!');
+  } catch (error) {
+    console.error('Error copying files:', error);
+  }
+}
+
+// Run the script
+copyTemplate();
---
+++ template/index.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas(windowWidth - 1, windowHeight - 4)
+}
+
+function draw() {
+  background(200)
+}
